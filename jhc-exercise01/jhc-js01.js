0b2a2b122dc26dcf02ca4158e2c9ea23657dcbb2
jswan <jswannieman@gmail.com>
2023-06-13 16:46:52 +1000

    Commit jhc-js01 Javascript Component

--- jhc-exercise01/jhc-js01.js
+++ jhc-exercise01/jhc-js01.js
@@ -1,20 +1,33 @@
 const conty = document.querySelector(".container");
 const butty = document.querySelector("button");
-let count = 0;
+let count1 = 0;
+let count2 = 0;
 
 const clicker = () => {
-  count++;
-  if (count === 1) {
+  count1++;
+  count2++;
+  if (count2 === 8) {
+    return;
+  }
+  if (count1 === 1) {
     butty.style.backgroundColor = "#000";
     butty.style.color = "#FFF";
   } else {
-    count = 0;
+    count1 = 0;
     butty.style.backgroundColor = "yellow";
     butty.style.color = "#000";
   }
   const pippie = document.createElement("p");
   pippie.textContent = "Button has been clicked";
+  pippie.setAttribute("class", "pippies");
   conty.appendChild(pippie);
+  if (count2 === 7) {
+    setTimeout(() => {
+      document.querySelectorAll(".pippies").forEach((pip) => {
+        pip.textContent = "Button Clicking has come to a Halt!";
+      });
+    }, 3000);
+  }
 };
 
 butty.onclick = clicker;
